49da733489116ecd0ee69073d866b0ed6407b4e9
Jeferson Luis <jgjefersonluis@gmail.com>
2020-07-26 01:19:08 -0300

    desafio print e input cadastro de procurado pela policia

---
+++ Desafio-Procurado/procurado.py
@@ -0,0 +1,31 @@
+print(f"PROCURADO PELA POLICIA EM TODO TERRITORIO NACIONAL")
+
+print(f"Paulo Cupertino Matias")
+
+print(f"Homicidio")
+
+def main():
+
+    data_nasc = ('Data de nascimento: 24/08/1970.')
+    idade_atual = ('Idade atual: 49 anos.')
+    nasceu = ('Natural de São Paulo - SP.')
+    sexo = ('Sexo: Masculino.')
+    pele = ('Pele: Branca.')
+    olhos = ('Olhos: Castanhos Escuros.')
+    cabelo = ('Cabelo: Castanhos Escuros.')
+    altura = ('Altura: 1,83 metros.')
+    tatu_cicatriz = ('Tatuagens e cicatrizes: Tatuagem frase no antebraço:'
+                     ' Marginal sempre Marginal.')
+
+    print()
+    print(data_nasc)
+    print(idade_atual)
+    print(nasceu)
+    print(sexo)
+    print(pele)
+    print(olhos)
+    print(cabelo)
+    print(altura)
+    print(tatu_cicatriz)
+
+main()
